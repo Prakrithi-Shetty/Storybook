ff57b8b6bd5ae4b1acfb474574d143b1d4219cbd
Prakrithi-Shetty <prakrithishetty8072@gmail.com>
2020-05-05 17:48:45 +0530

    commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,8 @@
-import { addDecorator, configure } from '@storybook/react';
-
-import { withThemesProvider } from 'storybook-addon-styled-component-theme';
-import ThemeProvider from './theme-provider';
-
-import theme from "../src/Components/Theme/Theme";
-
-const themes = [theme];
-addDecorator(withThemesProvider(themes, ThemeProvider));
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
